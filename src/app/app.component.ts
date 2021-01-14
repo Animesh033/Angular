9ec8ec4597fd73f5e2284684d9e83cba8d2587cd
Animesh <animesh.ultimate@gmail.com>
2021-01-15 05:03:25 +0530

    Todo App based on angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { Todo } from "./todo";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lco-angular';
+
+  /**
+   * 
+   * 
+    date = new Date();
+    constructor() {
+    console.log("C called");
+    this.doSomething("Animesh")
+  }
+
+  doSomething(val:string): void {
+    val = "Awesome";
+  }*/
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id: number) {
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
